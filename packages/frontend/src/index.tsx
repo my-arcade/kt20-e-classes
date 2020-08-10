a7cfd68e8589ce2a588709cdc2cd09ea50576abd
Kcharles52 <kcharles52@gmail.com>
2020-08-10 19:51:10 +0300

    split config for webpack

--- packages/frontend/src/index.tsx
+++ packages/frontend/src/index.tsx
@@ -3,4 +3,5 @@ import React from 'react';
 import App from './App.wrapper';
 
 const wrapper = document.getElementById("app"); 
+
 wrapper ? ReactDOM.render(<App />, wrapper) : false;
--- packages/frontend/webpack.common.js
+++ packages/frontend/webpack.common.js
@@ -2,6 +2,7 @@
 
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const {transform} = require('@formatjs/ts-transformer');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const path = require('path');
 
 const intlTransformer = () => {
@@ -16,17 +17,6 @@ const intlTransformer = () => {
 }
 
 module.exports = {
-  devServer: {
-    // tell webpack all our assets can be get relative to the base path (/)
-    publicPath: '/',
-    // when requesting a page, the browser sends the server request with that specific url
-    // and then throws an error when the path is not found (e.g: refresh when on /course/123 -> 
-    // request to /course/213/index.html instead of /index.html)
-    // historyApiFallback: true instructs webpack to always get the /index.html file
-    historyApiFallback: true,
-    port: 3100,
-    contentBase: path.join(__dirname, '/src')
-  },
   resolve: {
     extensions: ['.js', '.ts', '.tsx', '.jsx'],
     alias: {
@@ -79,6 +69,7 @@ module.exports = {
     ]
   },
   plugins: [
+    new CleanWebpackPlugin(),
     new HtmlWebPackPlugin({
       template: path.join(__dirname, '/src/index.html'),
       filename: 'index.html'
